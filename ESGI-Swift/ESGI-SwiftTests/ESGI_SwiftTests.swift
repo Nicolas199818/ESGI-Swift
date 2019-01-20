aafa5b41899c41ea3b7852a4946876fdb4b36fbd
Nicolas THIBAULT <nicolasthibault@macbook-pro-de-nicolas.home>
2019-01-21 00:22:30 +0100

    initial commit

---
+++ ESGI-Swift/ESGI-SwiftTests/ESGI_SwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  ESGI_SwiftTests.swift
+//  ESGI-SwiftTests
+//
+//  Created by Nicolas THIBAULT on 21/01/2019.
+//  Copyright © 2019 Nicolas THIBAULT. All rights reserved.
+//
+
+import XCTest
+@testable import ESGI_Swift
+
+class ESGI_SwiftTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
